13571ace9282702bf6008d6e886a5ae689b74e8a
yongho0720 <roh.yh0720@gmail.com>
2026-08-21 01:01:45 +0900

    Restore project images without frames

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -12,5 +12,8 @@ export function ProjectSection({ project, reverse = false }: { project: Project;
       <div className="badge-list">{project.stack.map(item => <TechBadge key={item}>{item}</TechBadge>)}</div>
       {project.url ? <a className="text-link" href={project.url} target="_blank" rel="noreferrer">VISIT PROJECT <span>↗</span></a> : <span className="text-link disabled">CURRENTLY IN INTERNAL TEST</span>}
     </div>
+    <figure className="project-image reveal">
+      <img src={`${import.meta.env.BASE_URL}${project.image}`} alt={project.imageAlt} loading="lazy" />
+    </figure>
   </article>
 }
